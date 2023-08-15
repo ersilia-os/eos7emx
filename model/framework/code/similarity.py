045e3ca39a6677d11f007739b2a140f24b1a1d2c
Samuel Maina <samuelmayna@gmail.com>
2023-08-15 13:43:32 +0300

    Add length property.

--- model/framework/code/similarity.py
+++ model/framework/code/similarity.py
@@ -1,7 +1,3 @@
-from rdkit import Chem
-from rdkit.Chem.Descriptors import MolWt
-
-
 from smallworld_api import SmallWorld
 
 sw = SmallWorld()
@@ -9,7 +5,9 @@ sw = SmallWorld()
 
 def zinc_similarity(smiles_list):
     results = []
+
     for smiles in smiles_list:
-        df = sw.search(smiles, dist=5, db=sw.ZINC_dataset)
-        results.append(df["smiles"].tolist())
+        df = sw.search(smiles, dist=5, db=sw.ZINC_dataset, length=100)
+        output = df["smiles"].tolist()
+        results.append(output)
     return results
